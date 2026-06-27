d208a32d98ad70a3ce91b3bfce67ef49783dae56
abcd <azmyasir250@gmail.com>
2026-06-27 18:07:24 +0500

    add env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBobUZUrjPa2yAUwbm8KLFkiGKY80SR2jg",
-  authDomain: "poultry-form-7425b.firebaseapp.com",
-  projectId: "poultry-form-7425b",
-  storageBucket: "poultry-form-7425b.firebasestorage.app",
-  messagingSenderId: "101705174558",
-  appId: "1:101705174558:web:b8fb2402aaa84d2d99a80f",
-  measurementId: "G-PQSN4NP300",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
